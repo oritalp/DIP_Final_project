4499d526568327c5b55f002753e55acc72fd3934
sheliH <hendelsh@post.bgu.ac.il>
2024-03-06 14:50:58 +0200

    test

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hi")
+import cv2
+import os
+import numpy as np
+
+
+cap = cv2.VideoCapture(0)
+while(True):
+    ret, frame = cap.read()
+
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
